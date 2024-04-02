281591d04b8940b644f6532df78eeb20c05416af
sandrocardona <sandrocardona41@gmail.com>
2024-04-02 12:27:58 +0200

    2 Abril

--- REACT/idiomas/src/App.js
+++ REACT/idiomas/src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {Component} from "react";
+import { Button } from 'reactstrap';
 
-function App() {
+function Boton(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Button color={props.color} onClick={() => props.mensaje()} outline>{props.idioma}</Button>
+  )
 }
+
+class App extends Component {
+  constructor(props){
+  super(props);
+    this.state = {
+      saludo: "Pulsa para saludar"
+    }
+  }
+
+  ingles(){
+    this.setState({saludo: "Hi there!"})
+  }
+
+  español(){
+    this.setState({saludo: "Hola, qué tal"})
+  }
+
+  aleman(){
+    this.setState({saludo: "Was geht Mann!"})
+  }
+
+  andalu(){
+    this.setState({saludo: "paza pisha"})
+  }
+  
+
+    render(){
+      return(
+        <div className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Idiomas</h1>
+          <h3>{this.state.saludo}</h3>
+          <Boton idioma='Español' mensaje={() => this.español()} color='danger'/> {' '}
+          <Boton idioma='Inglés' mensaje={() => this.ingles()} color='primary'/> {' '}
+          <Boton idioma='Alemán' mensaje={() => this.aleman()} color='warning'/> {' '}
+          <Boton idioma='Andalúz' mensaje={() => this.andalu()} color='success'/> {' '}
+        </div>
+      )
+    }
+};
 
 export default App;
