23b97eb660ab467877e5b757242501e6fb8940d1
sandrocardona <sandrocardona41@gmail.com>
2024-04-03 11:12:58 +0200

    3 Abril

--- REACT/4raya/src/App.js
+++ REACT/4raya/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { Component } from 'react';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
+const MapaBotones = (props)=>{
+
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    listaBotones: /*aquí almaceno los colores de los botones*/,
+    
+    listaColores: ["primary","danger"],
+    // tendrás que añadir más atributos al state como el turno...
+    }
+  }
+
+  componentWillMount() {
+  // Utilízalo si necesitas hacer algo antes de renderizar
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
