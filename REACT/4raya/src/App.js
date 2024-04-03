2b90e29a21524b49b9088426099d8f0532613ef7
sandrocardona <sandrocardona41@gmail.com>
2024-04-03 12:43:01 +0200

    3 Abril

--- REACT/4raya/src/App.js
+++ REACT/4raya/src/App.js
@@ -2,18 +2,33 @@ import React , { Component } from 'react';
 import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const MapaBotones = (props)=>{
+const MapaBotones = (props) => {
+  let tablero = [];
+/*   console.log(props.tablero); */
+  for(let i = 0; i < props.longitud; i++){
+    let fila = [];
+    for (let j = 0; j < props.longitud; j++) {
+      fila.push(<Button key={`${i}${j}`} outline />);
+    }
+/*     console.log(fila); */
+    tablero.push(<div key={i}>{fila}</div>);
+  }
 
+
+  return tablero;
 }
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    listaBotones: /*aquí almaceno los colores de los botones*/,
+    listaBotones: Array(9).fill("x"),
+    longitud:9,
     
     listaColores: ["primary","danger"],
     // tendrás que añadir más atributos al state como el turno...
+    turno: 1,
+
     }
   }
 
@@ -24,7 +39,8 @@ class App extends Component {
   render() {
     return (
     <div className="App">
-
+      <h1>4 en rayas</h1>
+      <MapaBotones tablero={this.state.listaBotones} longitud = {this.state.longitud} ></MapaBotones>
     </div>
     );
   }
