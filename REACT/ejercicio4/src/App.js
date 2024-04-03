6f7a8586b154eb7c304ae353ff5bf3b0047e7ad6
sandrocardona <sandrocardona41@gmail.com>
2024-04-03 11:00:20 +0200

    3 Abril

--- REACT/ejercicio4/src/App.js
+++ REACT/ejercicio4/src/App.js
@@ -1,25 +1,50 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from './componentes/Userlist';
+import UserForm from './componentes/Userform';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      users: [
+        {id: 1, name: "perico", email: "perico@myfpschool.com"},
+        {id: 2, name: "juanico", email: "juanico@myfpschool.com"},
+        {id: 3, name: "andrés", email: "andres@myfpschool.com"},
+      ]
+    }
+  }
+
+  handleOnAddUser (event) {
+    event.preventDefault();
+    let user = {
+      name : event.target.name.value,
+      email: event.target.email.value
+    };
+
+    let tmp = this.state.users;
+    tmp.push(user);
+    this.setState({
+      users: tmp
+    });
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Me mola Myfpschool</h2>
+      </div>
+      <div>
+        <p><strong>Añade usuarios</strong></p>
+        <UserList users={this.state.users}/>
+        <UserForm onAddUser = {(e) => this.handleOnAddUser(e)} />
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
