bde8f6c6c28811ab907bbc875d614fff60ec168e
sandrocardona <sandrocardona41@gmail.com>
2024-04-08 11:44:19 +0200

    8 abril

--- REACT/4raya/src/App.js
+++ REACT/4raya/src/App.js
@@ -1,29 +1,28 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/App.css';
 
 const MapaBotones = (props) => {
   let tablero = [];
-/*   console.log(props.tablero); */
-  for(let i = 0; i < props.tablero.length; i++){
+  /*   console.log(props.tablero); */
+  for (let i = 0; i < props.tablero.length; i++) {
     let fila = [];
     for (let j = 0; j < props.tablero[0].length; j++) {
-      let colorBoton = '';
       console.log(props.tablero[i][j])
-      if (props.tablero[i][j]==="x"){
+      if (props.tablero[i][j] === "x") {
         fila.push(<Button onClick={() => props.click(i, j)} key={`${i}${j}`} outline />);
       }
-      else if(props.tablero[i][j]==="1"){
+      else if (props.tablero[i][j] === "1") {
         fila.push(<Button color={"primary"} onClick={() => props.click(i, j)} key={`${i}${j}`} />);
       }
 
-      else if(props.tablero[i][j]==="0"){
+      else if (props.tablero[i][j] === "0") {
         fila.push(<Button color={"danger"} onClick={() => props.click(i, j)} key={`${i}${j}`} />);
       }
 
     }
-/*     console.log(fila); */
+    /*     console.log(fila); */
     tablero.push(<div key={i}>{fila}</div>);
   }
 
@@ -31,51 +30,57 @@ const MapaBotones = (props) => {
 }
 
 const BotonTurno = (props) => {
-  return(<Button className='btnTurno' color={props.color}>{"Turno de "}{props.players}</Button>)
+  return (<Button className='btnTurno' color={props.color}>{"Turno de "}{props.players}</Button>)
 }
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-//      listaBotones: Array(9).fill(Array(9).fill("x")),
+      //      listaBotones: Array(9).fill(Array(9).fill("x")),
       listaBotones: JSON.parse(JSON.stringify(Array(9).fill(Array(9).fill("x")))),
-      longitud:9,
-      listaColores: ["primary","danger"],
+      longitud: 9,
+      listaColores: ["primary", "danger"],
       // tendrás que añadir más atributos al state como el turno...
       turno: 1,
       players: "azul"
     }
-    
-  }
-  componentWillMount(){
 
   }
+  componentWillMount() {
 
-  clickar(i, j){
-    /* para la primera fila */
-    if(i === 0){
-      console.log(i + " " + j);
-      /* cambiar turno y color del btnTurno */
-      if(this.state.turno === 1 && this.state.listaBotones[i][j]=="x"){
-        this.setState({players: "rojo"})
-        this.setState({turno: 0})
-        this.state.listaBotones[i][j]="1";
-      } 
-      else if(this.state.turno === 0 && this.state.listaBotones[i][j]=="x"){
-        this.setState({players: "azul"})
-        this.setState({turno: 1})
-        this.state.listaBotones[i][j]="0";
-      }
+  }
 
-    } else {
-      console.log(i + " " + j);
+  buscarCasilla(col){
+    for (let i=8; i>=0; i--){
+      if (this.state.listaBotones[i][col]=="x") return i;
     }
+    return -1;
+  }
+
+
+  clickar(i, j) {
+    /* para la primera fila */
+    let l = this.state.listaBotones
+    if (i !== 0) return;
+    if (this.buscarCasilla(j)==-1) return;
+
+    console.log(i + " " + j);
+    /* cambiar turno y color del btnTurno */
+    if (this.state.turno === 1 && this.state.listaBotones[i][j] == "x") {
+      l[this.buscarCasilla(j)][j] = "1";
+      this.setState({listaBotones:l, players:"rojo",turno:0})
+    }
+    else if (this.state.turno === 0 && this.state.listaBotones[i][j] == "x") {
+      l[this.buscarCasilla(j)][j] = "0";
+      this.setState({listaBotones:l,players:"azul",turno:1})
+    }
+
 
   }
 
   montador() {
-  // Utilízalo si necesitas hacer algo antes de renderizar
+    // Utilízalo si necesitas hacer algo antes de renderizar
   }
 
   render() {
@@ -83,11 +88,11 @@ class App extends Component {
     const colorBotonTurno = this.state.turno === 1 ? this.state.listaColores[0] : this.state.listaColores[1];
 
     return (
-    <div className="App">
-      <h1 id="titulo">4 en rayas</h1>
-      <MapaBotones tablero={this.state.listaBotones} longitud={this.state.longitud} click={(i, j) => this.clickar(i, j)}></MapaBotones>
-      <BotonTurno color={colorBotonTurno} players={this.state.players} ></BotonTurno>
-    </div>
+      <div className="App">
+        <h1 id="titulo">4 en rayas</h1>
+        <MapaBotones tablero={this.state.listaBotones} longitud={this.state.longitud} click={(i, j) => this.clickar(i, j)}></MapaBotones>
+        <BotonTurno color={colorBotonTurno} players={this.state.players} ></BotonTurno>
+      </div>
     );
   }
 }
