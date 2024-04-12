ad71b5d868648cd9a026164e849bc4029380ad98
sandrocardona <sandrocardona41@gmail.com>
2024-04-12 11:42:27 +0200

    12 Abril

--- REACT/fototipos/fototipos/src/App.js
+++ REACT/fototipos/fototipos/src/App.js
@@ -11,30 +11,59 @@ class App extends Component {
     this.state = {
       titulo: "Fototipos",
       puntuacion: 0,
+      respuestas: [],
+      completado: false,
     }
 
     this.puntuacion = this.puntuacion.bind(this);
     this.clickar = this.clickar.bind(this);
   }
 
-  puntuacion(puntos){
-    let total = this.state.puntuacion + puntos;
-    console.log("Puntuacion total: " + total)
-    this.setState({puntuacion: total})
+  reiniciar(){
+    let completadoAux = false;
+    let respuestasAux = [];
+    let puntuacionAux = 0;
+    this.setState({completado: completadoAux, respuestas: respuestasAux, puntuacion: puntuacionAux})
   }
 
-  clickar(puntos){
+  puntuacion(puntos, index){
+    // Variables
+    let respuestasAux = this.state.respuestas;
+    let total = this.state.puntuacion;
+
+    // Incluir respuesta
+    if(!respuestasAux.includes(index)){
+      respuestasAux.push(index);
+      total+=puntos;
+    }
+
+    console.log("Puntuacion total: " + total)
+    console.log("respuestas: " + respuestasAux)
+
+    if(this.state.respuestas.length > 6)
+      this.setState({puntuacion: total, respuestas: respuestasAux, completado: true})
+    else
+      this.setState({puntuacion: total, respuestas: respuestasAux})
+  }
+
+  clickar(puntos, index){
     console.log("Puntos " + puntos);
-    this.puntuacion(puntos);
+    this.puntuacion(puntos, index);
+    console.log(index);
   }
 
   render() {
-    return (
+    return this.state.completado ? (
+      <div className='App'>
+        <p>respuesta</p>
+        <Button onClick={() => this.reiniciar()} className='btnReiniciar'>Reiniciar</Button>
+      </div>
+    ) : (
       <div className="App">
         <h1 id="titulo">{this.state.titulo}</h1>
         <Questionario clickar={this.clickar}/>
       </div>
-    );
+    )
   }
 }
 
--- REACT/fototipos/fototipos/src/componentes/Questionario.js
+++ REACT/fototipos/fototipos/src/componentes/Questionario.js
@@ -1,19 +1,39 @@
 import React from "react";
 import FOTOTIPO from "./fototipos";
 import { Button } from 'reactstrap';
+import { useState } from "react";
+
+
+
+
+
 
 
 const Questionario = (props) => {
+    const [color, setColor] = useState(null);
+
+    const handleColor = (indice) => {
+        setColor(indice);
+    }
+
     return(
         <div>
             {FOTOTIPO.map(e => (
                 <>
                 <p key = {e.orden}>{e.orden}{". "}{e.texto}</p>
                 <ul>
-                {e.respuestas.map(r => (
-                <li key={e.orden + " " + r.puntos}>
-                    <Button puntos={r.puntos} onClick={() => props.clickar(r.puntos)} color="info" outline>{r.valor}</Button>
-                </li>
+                {e.respuestas.map((r, answerIndex) => (
+                    <Button 
+                            key={answerIndex}
+                            puntos={r.puntos}
+                            onClick={() => {
+                                        props.clickar(r.puntos, e.orden, answerIndex);
+                                        handleColor(answerIndex);
+                                    }}
+                            color={color === answerIndex ? "primary" : "secondary"}
+                    >
+                    {r.valor}
+                    </Button>
                 ))}
                 </ul>
                 </>
