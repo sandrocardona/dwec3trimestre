ffe0e2eb3d0eb77ec9369fa850ba13d2a92ef7d6
sandrocardona <sandrocardona41@gmail.com>
2024-05-19 23:45:54 +0200

    s

--- REACT/inmobiliaria/src/App.js
+++ REACT/inmobiliaria/src/App.js
@@ -1,26 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const UploadPropertie = (props) => {
+  return <Button outline>Subir anuncio</Button>
+}
+
+const SearchEngine = (props) => {
+  return <>
+    <div className='search-engine'>
+      <h2>{props.slogan}</h2>
+      <form>
+        {/* Aquí irá bucle para traer tipo_venta */}
+        <select>
+          <option>comprar</option>
+          <option>alquilar</option>
+          <option>compartir</option>
+        </select>
+        {/* Aquí irá bucle para traer tipo_propiedad */}
+        <select>
+          <option>piso</option>
+          <option>casa</option>
+          <option>chalet</option>
+        </select>
+        <input type='text' placeholder='Buscar localidad'></input>
+        <Button>Buscar</Button>
+      </form>
     </div>
-  );
+  </>
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slogan: "Buscar propiedad"
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Inmobiliaria<img src={logo} className="App-logo" alt="logo" /></h1>
+          <UploadPropertie />
+        </header>
+        <SearchEngine slogan={this.state.slogan} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
